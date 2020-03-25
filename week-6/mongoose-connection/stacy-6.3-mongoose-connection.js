af49783198c36495e57a1c703bb08d768ef54dde
Clayton Stacy <clayton.stacy@gmail.com>
2020-03-25 17:14:27 -0500

    Finishing 6.3

---
+++ week-6/mongoose-connection/stacy-6.3-mongoose-connection.js
@@ -0,0 +1,37 @@
+
+/*
+============================================
+Title: Assignment 6.3
+Author: Professor Krasso
+Date: 23 March 2020
+Modified By: Clayton Stacy
+Description: Mongoose Connection
+============================================
+*/
+
+
+var express = require('express');
+var http = require('http');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb://admin:admin@ds213239.mlab.com:13239/krasso-ems'
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connected error: "));
+
+db.once("open", function() {
+    console.log("Application connected to mLab MongoDB instance");
+});
+
+var app = express();
+app.use(logger('dev'));
+http.createServer(app).listen(5000, function() {
+    console.log('Application started and listening on port 5000');
+});
